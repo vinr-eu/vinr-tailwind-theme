ccf351e66968fcefe977d122fe56e17aeec28a68
Richy Great <richygreat@gmail.com>
2024-04-07 21:57:42 +0200

    UTM added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+function toggleMenu() {
   // Select the menu toggle button and the menu container
   const menuOpen = document.querySelector(".menu-open");
   const menuClose = document.querySelector(".menu-close");
@@ -17,4 +17,44 @@ document.addEventListener("DOMContentLoaded", () => {
       mobileMenu.classList.toggle("hidden");
     });
   }
+}
+
+function storeUTMParameters() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const utmParams = [
+    "utm_source",
+    "utm_medium",
+    "utm_campaign",
+    "utm_term",
+    "utm_content",
+  ];
+
+  utmParams.forEach((param) => {
+    const value = queryParams.get(param);
+    if (value) localStorage.setItem(param, value);
+  });
+}
+
+function fillHiddenUTMFields() {
+  const utmParams = [
+    "utm_source",
+    "utm_medium",
+    "utm_campaign",
+    "utm_term",
+    "utm_content",
+  ];
+
+  utmParams.forEach((param) => {
+    const element = document.getElementById(param);
+    const value = localStorage.getItem(param);
+    if (element && value) {
+      element.value = value;
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  toggleMenu();
+  storeUTMParameters();
+  fillHiddenUTMFields();
 });
